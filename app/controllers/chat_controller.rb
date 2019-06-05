0448866e3dbbfa65187e0570450b39093aa9ec20
daleksprinter <b1015120@fun.ac.jp>
2019-06-06 08:53:20 +0900

    get joinned rooms by current user

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,5 +1,7 @@
 class ChatController < ApplicationController
   before_action :authenticate_user!
   def index
+    user = User.find_by(name: current_user.name)
+    @tmp = Room.find_by_sql(['select * from joins left outer join rooms on joins.room_id = rooms.id where joins.user_id = ?', user.id])
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,4 @@
 class Room < ApplicationRecord
+    has_many :joins, dependent: :destroy
+    has_many :joins, dependent: :destroy
 end
