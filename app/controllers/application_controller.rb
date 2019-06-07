59b85eb9394c353a0d7754d37ced9a920b94c9b5
daleksprinter <b1015120@fun.ac.jp>
2019-06-07 14:12:36 +0900

    get rooms of login user in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :get_room_current_user
+    def get_room_current_user
+        user = User.find_by(name: current_user.name)
+        sql = 'select * 
+            from joins left outer join rooms on joins.room_id = rooms.id 
+            where joins.user_id = ?'
+        @rooms = Room.find_by_sql([sql, user.id])
+    end
 end
--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,7 +1,12 @@
 class ChatController < ApplicationController
   before_action :authenticate_user!
   def index
-    user = User.find_by(name: current_user.name)
-    @tmp = Room.find_by_sql(['select * from joins left outer join rooms on joins.room_id = rooms.id where joins.user_id = ?', user.id])
+  end
+  
+  before_action :authenticate_user!
+  def show
+    room_id = params[:id]
+
+
   end
 end
