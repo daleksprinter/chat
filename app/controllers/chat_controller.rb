caa0fae5ea9615fffc2893979796c5f1a583cc85
daleksprinter <b1015120@fun.ac.jp>
2019-06-07 16:13:52 +0900

    modify show controller

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -5,11 +5,11 @@ class ChatController < ApplicationController
   
   before_action :authenticate_user!
   def show
-    @room_id = params[:id]
-    room = Room.find_by(room_id: @room_id)
+    room_id = params[:id]
+    @room = Room.find_by(room_id: room_id)
     sql = 'select * 
             from users join messages on messages.user_id = users.id 
             where messages.room_id = ?'
-    @messages = User.find_by_sql([sql, room.id])
+    @messages = User.find_by_sql([sql, @room.id])
   end
 end
