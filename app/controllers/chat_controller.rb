140fa9be2a8a92eea5f834a207923b2a0796f7a0
daleksprinter <b1015120@fun.ac.jp>
2019-06-07 14:26:11 +0900

    get messages by room id parameter

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -6,7 +6,7 @@ class ChatController < ApplicationController
   before_action :authenticate_user!
   def show
     room_id = params[:id]
-
-
+    room = Room.find_by(room_id: room_id)
+    @messages = Message.where(room_id: room[:id])
   end
 end
