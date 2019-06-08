a9ddb80eeef531d5c08a48cf201b1acd13608bf9
daleksprinter <b1015120@fun.ac.jp>
2019-06-08 15:03:01 +0900

    modify singin form and add user hoge3, hoge4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:user_id])
+    end
+
     before_action :get_room_current_user
     def get_room_current_user
         @rooms = []
